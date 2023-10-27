b784e493d53cf33aa02c500488afccebbc7ee47b
BlacHera <121027660+BlacHera@users.noreply.github.com>
2023-10-27 17:49:48 +0200

    Added axios intergration to show the weather of the city

---
+++ src/WeatherSearch.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function WeatherSearch() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "3c92e732d2a8056b206186669c0852af";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function changeCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form className="search-form" onSubmit={handleSubmit}>
+      <div className="row">
+        <div className="col-9">
+          <input
+            type="search"
+            placeholder="Enter a city.."
+            className="form-control search-input"
+            onChange={changeCity}
+          />
+        </div>
+        <div className="col-3 p-0">
+          <input
+            type="submit"
+            className="btn btn-primary w-100"
+            value="Search"
+          />
+        </div>
+      </div>
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div className="WeatherSearch">
+        {form}
+        <ul>
+          <li> {Math.round(weather.temperature)}°C</li>
+          <li>{weather.description}</li>
+          <li>
+            Humidity: {weather.humidity}% Wind: {weather.wind}km/h
+          </li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
