1466cee365aaf5b08a7763727cf9fb58fd1432ef
BlacHera <121027660+BlacHera@users.noreply.github.com>
2023-10-28 18:33:35 +0200

    Moved the small element from the index html to App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
   return (
     <div className="App">
        <Weather temperature={26} />
-    
+     <small>
+    <a href="https://github.com/BlacHera/react-weather-app" target="_blank"
+    >Open-source code </a> 
+    by Phyllis Ampo
+      </small>
     </div>
   );
 }
