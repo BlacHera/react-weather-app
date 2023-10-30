0fffff397b85dd0188aa0450ab3c65960c5f25af
BlacHera <121027660+BlacHera@users.noreply.github.com>
2023-10-30 09:34:52 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
+      <div className="container">
        <Weather temperature={26} />
      <small>
     <a href="https://github.com/BlacHera/react-weather-app" target="_blank" rel="noreferrer"
     >Open-source code </a> 
     by Phyllis Ampo
       </small>
+      </div>
     </div>
   );
 }
