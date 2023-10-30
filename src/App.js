b265f7d26b791b4493e7810809a2fca4581fe8d4
BlacHera <121027660+BlacHera@users.noreply.github.com>
2023-10-30 13:24:30 +0100

    Added footer to file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from "react";
 import './App.css';
-
 import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-       <Weather temperature={26} />
-     <small>
+       <Weather defualtCity="Madrid" />
+     <footer>
+       This project was coded by Phyllis Ampo and is{" "} 
     <a href="https://github.com/BlacHera/react-weather-app" target="_blank" rel="noreferrer"
-    >Open-source code </a> 
-    by Phyllis Ampo
-      </small>
+    >Open-sourced on Github </a> {" "}
+          and hosted on {" "}
+          <a
+            href="https://endearing-bonbon-f53412.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          Netlify
+          </a>
+      </footer>
       </div>
     </div>
   );
