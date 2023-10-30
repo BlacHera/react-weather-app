cc88418f13cbba0e633d341134a60421c7d3a869
BlacHera <121027660+BlacHera@users.noreply.github.com>
2023-10-30 13:23:00 +0100

    Improved the elements in the component

--- src/Weather.js
+++ src/Weather.js
@@ -1,67 +1,55 @@
-import React, { useState } from "react";
-import WeatherSearch from "./WeatherSearch";
-
+import React from "react";
 import "./App.css";
 import "./Weather.css";
 
-export default function Weather(props) {
-  let [temperature, setTemperature] = useState(props.temperature);
-
-  function showFarehrenheit(event) {
-    event.preventDefault();
-    setTemperature(Math.round((props.temperature * 9) / 5 + 32));
-  }
-
-  let weatherData = {
-    city: "",
-    temperature: 26,
-    imgUrl: ""
-  };
-
+export default function Weather() {
   return (
     <div className="Weather">
-      <div className="card">
-        <WeatherSearch />
-        <div className="WeatherInfo">
-          <div className="row">
-            <div className="col-6">
-              <h1>{weatherData.city}</h1>
-              <ul>
-                <li>{weatherData.date}</li>
-              </ul>
-            </div>
-            <div className="col-lg-6 col-md-6 col-sm-8">
-              <div className="temperature-container d-flex justify-content-end">
-                <img
-                  src={weatherData.imgUrl}
-                  alt={weatherData.description}
-                  className="float-left"
-                />
-                <div className="temperature">
-                  <strong> {temperature} </strong>{" "}
-                  <span className="units">
-                    <a href="/">°C </a>|{" "}
-                    <a href="/" onClick={showFarehrenheit}>
-                      °F
-                    </a>
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
+        <form onSubmit="">
+      <div className="row">
+        <div className="col-9">
+          <input
+            type="search"
+            placeholder="Enter a city.."
+            className="form-control"
+            onChange=""
+            autoFocus="on"
+          />
         </div>
-        <div className="WeatherForecast row" id="forecast">
-          <div className="col-2">
-            <div className="WeatherForecastPreview">
-              <div className="forecast-time"></div>
-              <div className="forecast-temperature">
-                <span className="forecast-temperature-max"></span>
-                <span className="forecast-temperature-min"></span>
-              </div>
-            </div>
-          </div>
+        <div className="col-3 p-0">
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-primary w-100"
+          />
         </div>
       </div>
-    </div>
+    </form>
+        <h1>Barcelona</h1>
+              <ul>
+                <li>Monday 9:47</li>
+                <li>Light rain showers</li>
+              </ul>
+          <div className="row mt-3">
+            <div className="col-6">
+              <div className="clearfix">
+               <img
+                  src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
+                  alt="Light rain showers"
+                  className="float-left"
+                />
+                 <span className="temperature">18</span>
+                  <span className="units">°C</span>
+                  </div>
+             </div>
+            <div className="col-6">
+              <ul>
+                <li>Percipitation: 92%</li>
+                <li>Humidity: 69%</li>
+                <li>Wind: 5km/h</li>
+              </ul> 
+              </div>
+          </div>
+      </div>
   );
 }
